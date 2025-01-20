c28668e42f6919aa7e8a9016908ed19da65031dd
OsantiagoPerezE <ospereze98@gmail.com>
2025-01-20 17:02:47 -0500

    add localstorage

--- react/TestCustom/index.tsx
+++ react/TestCustom/index.tsx
@@ -1,9 +1,5 @@
 import React, { Fragment, useEffect, useMemo } from "react";
-import { useLazyQuery, useMutation } from "react-apollo";
 import { useOrderForm } from "vtex.order-manager/OrderForm";
-import GET_DOCUMENT from "./gql/getDocuments.gql";
-import CREATE_DOCUMENT from "./gql/createDocument.gql";
-import UPDATE_DOCUMENT from "./gql/updateDocument.gql";
 
 interface OrderFormItem {
   productId: string;
@@ -14,9 +10,6 @@ interface OrderFormItem {
 
 const TestCustom = () => {
   const { orderForm } = useOrderForm();
-  const [getDocuments, { data: dataGetDocument }] = useLazyQuery(GET_DOCUMENT);
-  const [createDocument] = useMutation(CREATE_DOCUMENT);
-  const [updateDocument] = useMutation(UPDATE_DOCUMENT);
 
   const id = useMemo(() => {
     return orderForm?.id || "";
@@ -27,73 +20,32 @@ const TestCustom = () => {
   }, [orderForm]);
 
   useEffect(() => {
-    if (id != "" && id != "default-order-form" && items.length > 0) {
-      getDocuments({
-        variables: {
-          acronym: "TE",
-          fields: ["orderFormId", "Items"],
-          where: `orderFormId=${id}`,
-        },
-      });
+    if (id !== "" && id !== "default-order-form" && items.length > 0) {
+      const storedData = localStorage.getItem("test-elektra");
+      const allOrderForms = storedData ? JSON.parse(storedData) : {};
+
+      const orderFormKey = `orderForm_${id}`;
+      const newOrderForm = {
+        orderFormId: id,
+        items: items.map((item: OrderFormItem) => ({
+          productId: item.productId,
+          sellingPrice: item.sellingPrice,
+          name: item.name,
+          quantity: item.quantity,
+        })),
+      };
+
+      if (!allOrderForms[orderFormKey]) {
+        console.log("CREATE en localStorage");
+        allOrderForms[orderFormKey] = newOrderForm;
+      } else {
+        console.log("UPDATE en localStorage");
+        allOrderForms[orderFormKey] = newOrderForm;
+      }
+
+      localStorage.setItem("list-orderforms", JSON.stringify(allOrderForms));
     }
   }, [items, id]);
-
-  useEffect(() => {
-    if (
-      dataGetDocument != undefined &&
-      id != "" &&
-      id != "default-order-form"
-    ) {
-      const responseGet = dataGetDocument?.documents || [];
-
-      if (responseGet.length === 0) {
-        console.log("CREATE");
-        createDocument({
-          variables: {
-            acronym: "TE",
-            document: {
-              fields: [
-                { key: "orderFormId", value: id },
-                {
-                  key: "Items",
-                  value: JSON.stringify(
-                    items.map((item: OrderFormItem) => ({
-                      productId: item.productId,
-                      sellingPrice: item.sellingPrice,
-                      name: item.name,
-                      quantity: item.quantity,
-                    }))
-                  ),
-                },
-              ],
-            },
-          },
-        });
-      } else {
-        console.log("UPDATE");
-        updateDocument({
-          variables: {
-            acronym: "TE",
-            fields: [
-              { key: "id", value: responseGet[0]?.id },
-              { key: "orderFormId", value: id },
-              {
-                key: "Items",
-                value: JSON.stringify(
-                  items.map((item: OrderFormItem) => ({
-                    productId: item.productId,
-                    sellingPrice: item.sellingPrice,
-                    name: item.name,
-                    quantity: item.quantity,
-                  }))
-                ),
-              },
-            ],
-          },
-        });
-      }
-    }
-  }, [dataGetDocument]);
 
   return <Fragment />;
 };
